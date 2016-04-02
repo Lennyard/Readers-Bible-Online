c88a46c2887c342017a050c3d4e899281be4eca7
Sako Kassabian <skassabian@csmci.com>
2016-04-01 22:59:27 -0700

    so many changes... so lazy for a good comment. It works.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,34 @@
 $(function(){
+	var $forCreator = $(".for-creator");
+	var $forBookmark = $(".for-bookmark");
+	
+	//show who made this
+	$(".creator").click(function(){
+		$forBookmark.slideUp("fast");
+		$forCreator.slideToggle("slow");
+	});
+
+	// show how to bookmark
+	$(".how-bookmark").click(function(){
+		$forCreator.slideUp("fast");
+		$forBookmark.slideToggle("slow")
+	});
+
+	//go to saved bookmark
+	$(".bookmark").click(function(){
+		getBookmark();
+	});
+
 	populateBooks();
 	// Sends AJAX request when drop down is changed
 	$(".books").on("click", "a", function(){	
 		var searchTerm = $(this).text();
-		getRequest(searchTerm);
+		populateText(searchTerm);
 		
 		});
 
 
-function getRequest(searchTerm) {
+function populateText(searchTerm) {
 		var params = {
 			p: searchTerm,
 			v: "nasb" // saint esv, where art thou?
@@ -17,22 +37,25 @@ function getRequest(searchTerm) {
 		
 		$.getJSON(url, params, function(data){
 			var html = "";
-			var html1 ="";
+			var counter = 1;
 			//grab each book
 			$.each(data.book, function(index, value){
+				
+				
 				//grab each chapter and verse
 				$.each(value.chapter, function(index, value){
-					console.log(value);
 					//if(index.verse )
-					html += value.verse + " "; 
+					html += "<span data-book="+ searchTerm + " data-verse=" + counter++ + ">" + value.verse + "</span> "; 
 				});
 			});
 			
+			$("#book-title").text(searchTerm);
 			$("#book-text").html(html);
+
 		});
 	}
 
-
+// populates the list of books from the select book dropdown
 function populateBooks(){
 	var params = {
 			v: "nasb" 
@@ -49,8 +72,55 @@ function populateBooks(){
 		});
 }
 
+$("#book-text").on("click", "span", function(){
+	var $this = $(this);
+	var verse = $this.data("verse");
+	var book = $this.data("book");
+	
+	//remove and add highlighting
+	$this.click(function(){
+		confirm("Bookmark this Verse?");
+		$("span").removeClass("selected")
+		$this.addClass("selected");
+	});
+	//remove and add Cookies to save location
+	Cookies.remove("saved-book");
+	Cookies.remove("saved-verse");
+	Cookies.set("saved-book", book);
+	Cookies.set("saved-verse", verse);
+});
 
+// get Cookie info.
+function getBookmark() {
+		var b = Cookies.get("saved-book");
+		var v = Cookies.get("saved-verse");
+		
+		populateText(b);
+		console.log(v);
 
+		setTimeout(function(){
+			$('html, body').animate({
+	        scrollTop: $("span[data-verse='"+ v + "']").offset().top - 75
+	    	});
+		}, 2000);
+		setTimeout(function(){
+			$("span[data-verse='"+ v + "']").addClass("selected");
+		}, 2500);
+	
+}
+// put year in footer
+var d = new Date();
+var y = d.getFullYear();
+$("footer").append("<h5>All Rights Reserved - Ankas Studios - " + y);
+/*
+//for the future - desktops
+function setGrid(e){
+	var $span = $("span").data();
+	for (var i in data){
+		$('<li>')
+	}
+}
+*/
 
 
 });
